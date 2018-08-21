b8a0573abf6a0642d708229bd8a7678d40613356
Dominggoes Isakh <drjisakh@gmail.com>
2018-08-21 22:18:57 +0200

    galaxys2-common: Use HWC2On1 adapter

--- common.mk
+++ common.mk
@@ -89,6 +89,7 @@ PRODUCT_PACKAGES += \
     AdvancedDisplay \
     android.hardware.graphics.allocator@2.0-impl-exynos4 \
     android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
     android.hardware.keymaster@3.0-impl \
     android.hardware.bluetooth@1.0-impl \
     libbt-vendor \
