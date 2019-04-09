7c797a18a1e4afe0ecbae48240b0f7efc240bea6
Shilin Victor <radicaldreamer00001@gmail.com>
2019-04-09 22:59:15 +0300

    galaxys2-common: use low-end video codecs
    thanks to @NoN1979 for idea
    
    Change-Id: I291d367f5d449bd9739655e4229592d9d5cdbafe

--- common.mk
+++ common.mk
@@ -253,7 +253,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
