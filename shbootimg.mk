e8367e1dbaa75f4cc1bfcd2117fb2002d41491f3
Dominggoes Isakh <drjisakh@gmail.com>
2019-11-30 01:14:11 +0100

    galaxys2-common: Fix shbootimg.mk
    Change-Id: I09c3878122af774738184cf0c135fc25a0249fb0

--- shbootimg.mk
+++ shbootimg.mk
@@ -16,42 +16,8 @@
 
 LOCAL_PATH := $(call my-dir)
 
-prebuilts_folder := device/samsung/galaxys2-common/prebuilts
-
-# Uncompressed boot ramdisk (make ramdisk)
-uncompressed_ramdisk := $(PRODUCT_OUT)/../../../../$(prebuilts_folder)/ramdisk.cpio
-$(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
-	zcat $< > $@
-	@echo "Made uncompressed ramdisk boot image: $@"
-
-.PHONY: ramdisk-galaxys2-common
-ramdisk-galaxys2-common: $(uncompressed_ramdisk)
-
-# Uncompressed recovery ramdisk (make recoveryimage)
-recovery_uncompressed_ramdisk_device := $(PRODUCT_OUT)/../../../../$(prebuilts_folder)/recovery-ramdisk-device.cpio
-$(recovery_uncompressed_ramdisk_device): $(MKBOOTFS) $(ADBD) \
-	    $(INTERNAL_ROOT_FILES) \
-	    $(INSTALLED_RAMDISK_TARGET) \
-            $(INSTALLED_BOOTIMAGE_TARGET) \
-	    $(INTERNAL_RECOVERYIMAGE_FILES) \
-	    $(recovery_initrc) $(recovery_sepolicy) \
-	    $(INSTALLED_2NDBOOTLOADER_TARGET) \
-	    $(INSTALLED_RECOVERY_BUILD_PROP_TARGET) \
-	    $(recovery_resource_deps) \
-	    $(recovery_fstab) \
-	    $(RECOVERY_INSTALL_OTA_KEYS) \
-	    $(BOARD_RECOVERY_KERNEL_MODULES) \
-	    $(DEPMOD)
-	$(call build-recoveryramdisk)
-	@echo ----- Making uncompressed recovery ramdisk ------
-	$(hide) $(MKBOOTFS) $(TARGET_RECOVERY_ROOT_OUT) > $@
-
-.PHONY: recoveryimage-galaxys2-common
-recoveryimage-galaxys2-common: $(recovery_uncompressed_ramdisk_device)
-
-# Take zImage as boot.img
 $(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET)
 	$(ACP) -fp $< $@
 
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_uncompressed_ramdisk) $(recovery_kernel)
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_uncompressed_ramdisk)
 	lzop -f9 -o $@ $<
