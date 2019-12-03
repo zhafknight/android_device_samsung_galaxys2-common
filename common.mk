efb13ecfcc4d7d3a2234d5e767f742521f09eeb8
Dominggoes Isakh <drjisakh@gmail.com>
2019-12-03 23:38:02 +0100

    galaxys2-common: Remove SamsungDoze
    Change-Id: I419cdee501421ed31665f5f547a7003748735d35

--- common.mk
+++ common.mk
@@ -201,9 +201,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     config.disable_atlas=true
 
-PRODUCT_PACKAGES += \
-    SamsungDoze
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     make_ext4fs \
