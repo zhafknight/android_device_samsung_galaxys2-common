94cefc34768a307417e8bf6aa8a805ddaf0dbd1a
Dominggoes Isakh <drjisakh@gmail.com>
2020-02-05 07:52:33 +0100

    galaxys2-common: Reset to GO configuration of Android Q

--- go_galaxys2-common.mk
+++ go_galaxys2-common.mk
@@ -16,18 +16,10 @@
 
 # Sets Android Go default values for properties specific for Galaxys2-common
 
-# Set lowram options
+# Set lowram options and enable traced by default
 PRODUCT_PROPERTY_OVERRIDES += \
      ro.config.low_ram=true \
-     ro.lmk.critical_upgrade=true \
-     ro.lmk.upgrade_pressure=40 \
-     ro.lmk.downgrade_pressure=60 \
-     ro.lmk.kill_heaviest_task=false \
-     ro.statsd.enable=false
-
-# set threshold to filter unused apps
-PRODUCT_PROPERTY_OVERRIDES += \
-     pm.dexopt.downgrade_after_inactive_days=10
+     persist.traced.enable=1
 
 
 # Speed profile services and wifi-service to reduce RAM and storage.
@@ -61,16 +53,53 @@ PRODUCT_PROPERTY_OVERRIDES += dalvik.vm.heapsize=360m
 # Do not generate libartd.
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
+# Do not spin up a separate process for the network stack on go devices, use an in-process APK.
+PRODUCT_PACKAGES += InProcessNetworkStack
+
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
 # leave less information available via JDWP.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
+# Disable Scudo outside of eng builds to save RAM.
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
+  PRODUCT_DISABLE_SCUDO := true
+endif
+
+# Set lowram options
+PRODUCT_PROPERTY_OVERRIDES += \
+     ro.lmk.critical_upgrade=true \
+     ro.lmk.upgrade_pressure=40 \
+     ro.lmk.downgrade_pressure=60 \
+     ro.lmk.kill_heaviest_task=false \
+     ro.statsd.enable=true
+
+# set threshold to filter unused apps
+PRODUCT_PROPERTY_OVERRIDES += \
+     pm.dexopt.downgrade_after_inactive_days=10
+
+# set the compiler filter for shared apks to quicken.
+# Rationale: speed has a lot of dex code expansion, it uses more ram and space
+# compared to quicken. Using quicken for shared APKs on Go devices may save RAM.
+# Note that this is a trade-off: here we trade clean pages for dirty pages,
+# extra cpu and battery. That's because the quicken files will be jit-ed in all
+# the processes that load of shared apk and the code cache is not shared.
+# Some notable apps that will be affected by this are gms and chrome.
+# b/65591595.
+PRODUCT_PROPERTY_OVERRIDES += \
+     pm.dexopt.shared=quicken
+
+# Default heap sizes. Allow up to 256m for large heaps to make sure a single app
+# doesn't take all of the RAM.
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=128m
+    dalvik.vm.heapsize=256m
+
 # 512MB specific properties.
 
 # lmkd can kill more now.
 PRODUCT_PROPERTY_OVERRIDES += \
-     ro.lmk.medium=700 \
+     ro.lmk.medium=700
 
 # madvise random in ART to reduce page cache thrashing.
 PRODUCT_PROPERTY_OVERRIDES += \
