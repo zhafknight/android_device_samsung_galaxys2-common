0a1f829f39f73f1cb25951180ad81689d9f68db9
Dominggoes Isakh <drjisakh@gmail.com>
2020-03-25 00:02:41 +0100

    galaxys2-common: Update Magisk kernel build-script for Android Q

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -166,11 +166,7 @@ BOARD_USES_FULL_RECOVERY_IMAGE := true
 
 # Boot.img
 BOARD_CUSTOM_BOOTIMG := true
-ifeq ($(WITH_MAGISKRAMDISK),true)
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/galaxys2-common/shbootimg_magisk.mk
-else
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/galaxys2-common/shbootimg.mk
-endif
 
 # Override healthd HAL
 BOARD_HAL_STATIC_LIBRARIES := libhealthd.exynos4
