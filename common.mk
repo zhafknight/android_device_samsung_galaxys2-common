ffaa32a231ba04301464262f20a902cdfe3551a0
Shilin Victor <chrono.monochrome@gmail.com>
2020-10-01 15:39:50 +0300

    galaxys2-common: upgrade Audio HIDL to 6.0
    Change-Id: Ibe778dfb9360ad6c58c774ae93b0124c001b9ee9

--- common.mk
+++ common.mk
@@ -114,8 +114,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio Packages
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     audio.primary.exynos4 \
     audio.a2dp.default \
     audio.r_submix.default \
