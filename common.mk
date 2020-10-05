27f1911df61a89680b0519912cad9e3968b0c7ae
Dominggoes Isakh <drjisakh@gmail.com>
2020-10-05 22:44:11 +0200

    galaxys2-common: Use Google's Wifi HIDL service

--- common.mk
+++ common.mk
@@ -251,7 +251,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service.legacy \
+    android.hardware.wifi@1.0-service \
     wificond \
     hostapd \
     libwpa_client \
