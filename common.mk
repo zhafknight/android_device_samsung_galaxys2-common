5018dd9a48a852d56a0011fad38ca6a27c01d2fc
Dominggoes Isakh <drjisakh@gmail.com>
2020-11-13 22:42:23 +0100

    Revert "galaxys2-common: Use Google's Wifi HIDL service"
    This reverts commit 27f1911df61a89680b0519912cad9e3968b0c7ae.

--- common.mk
+++ common.mk
@@ -260,7 +260,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     wificond \
     hostapd \
     libwpa_client \
