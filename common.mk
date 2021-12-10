c7432dd8a209453af488ea627d19d5dd187edd87
Dominggoes Isakh <drjisakh@gmail.com>
2021-12-11 00:13:08 +0100

    galaxys2-common: Uprev Audio HAL to 7.0

--- common.mk
+++ common.mk
@@ -154,8 +154,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio Packages
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.audio.service \
     audio.primary.exynos4 \
