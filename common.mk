ae6d091738f83bd5c2beab0d7ac85ae8c8a284e5
Dominggoes Isakh <drjisakh@gmail.com>
2022-10-28 20:32:49 +0200

    Revert "galaxys2-common: Bringup T: Drop audio.a2dp.default"
    This reverts commit 937b9a00adf22338fb48e0a3cc6a0606f1f96a2a.

--- common.mk
+++ common.mk
@@ -167,6 +167,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-service \
     android.hardware.audio.service \
     audio.primary.exynos4 \
+    audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudiohw_legacy
