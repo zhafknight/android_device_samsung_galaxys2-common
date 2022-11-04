cb91f28a9319f5975fc88ca5d7c89fbf84b89ab1
Dominggoes Isakh <drjisakh@gmail.com>
2022-11-04 20:40:42 +0100

    galaxys2-common: Remove missing Live Wallpapers

--- common.mk
+++ common.mk
@@ -332,14 +332,7 @@ PRODUCT_PACKAGES += \
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
     LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
-    VisualizationWallpapers \
     librs_jni
 
 # Wifi
