1895dc28b4b61d4ec1880bd6d4883a35a77f2e0c
IRonSquidist <96339831+zhafknight@users.noreply.github.com>
2026-08-06 01:44:07 +0000

    galaxys2-common: Drop Exynos LiveDisplay HAL

--- common.mk
+++ common.mk
@@ -171,7 +171,6 @@ PRODUCT_PACKAGES += \
 
 # HAL
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.samsung-exynos \
     android.hardware.graphics.allocator@2.0-impl-exynos4 \
     android.hardware.graphics.mapper@2.0-impl-exynos4 \
     android.hardware.graphics.composer@2.1-impl \
