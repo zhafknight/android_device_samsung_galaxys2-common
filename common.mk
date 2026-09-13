8f2d20013d1cf64d24c60f19e8819e88fd302e78
zhafknight <zhafmazran@gmail.com>
2026-09-14 00:17:08 +0800

    galaxys2-common: Add default audio effects

--- common.mk
+++ common.mk
@@ -93,7 +93,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/media/libeffects/data/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml 
 
 # Wifi
 PRODUCT_COPY_FILES += \
